7e4036bfb0e51a69c831e21365eec7ede2190446
serya012 <ayrescarneiro01@gmail.com>
2024-03-21 13:51:52 -0300

    projetos

---
+++ src/app/projeto/projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projeto',
+  standalone: true,
+  imports: [],
+  templateUrl: './projeto.component.html',
+  styleUrl: './projeto.component.scss'
+})
+export class ProjetoComponent {
+
+}
