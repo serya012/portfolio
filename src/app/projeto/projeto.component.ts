bbb275db5010d6330d11c4a2fe3636a74018290c
serya012 <ayrescarneiro01@gmail.com>
2024-07-03 19:19:15 -0300

    material cards

--- src/app/projeto/projeto.component.ts
+++ src/app/projeto/projeto.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-projeto',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './projeto.component.html',
-  styleUrl: './projeto.component.css'
+  styleUrls: ['./projeto.component.css']
 })
 export class ProjetoComponent {
-
+  projects = [
+    {
+      name: 'Projeto 1',
+      description: 'Descrição do Projeto 1',
+      details: 'Mais detalhes sobre o Projeto 1'
+    },
+    {
+      name: 'Projeto 2',
+      description: 'Descrição do Projeto 2',
+      details: 'Mais detalhes sobre o Projeto 2'
+    }
+    // Adicione mais projetos conforme necessário
+  ];
 }
