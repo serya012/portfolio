10073322bc0388175d7fdab84802d3d686cf3d0d
serya012 <ayrescarneiro01@gmail.com>
2024-07-03 22:59:06 -0300

    card component

--- src/app/projeto/projeto.component.ts
+++ src/app/projeto/projeto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { MatCardModule } from '@angular/material/card';
@@ -10,25 +10,43 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './projeto.component.html',
   styleUrls: ['./projeto.component.scss']
 })
-export class ProjetoComponent {
+export class ProjetoComponent implements OnInit {
   projects = [
     {
-      name: 'Projeto 1',
+      name: 'Relógio Digital',
       description: 'Descrição do Projeto 1',
-      details: 'Mais detalhes sobre o Projeto 1'
+      details: 'Mais detalhes sobre o Projeto 1',
+      imageUrl: 'assets/photo01.jpg',
+      projectUrl: 'https://serya012.github.io/relogio/'
     },
     {
       name: 'Projeto 2',
       description: 'Descrição do Projeto 2',
-      details: 'Mais detalhes sobre o Projeto 2'
+      details: 'Mais detalhes sobre o Projeto 2',
+      imageUrl: 'path/to/image2.jpg',
+      projectUrl: 'https://link-para-seu-projeto-2.com'
     },
-    // Adicione mais projetos conforme necessário
+    // Adicione mais projetos conforme necessário até completar 10
   ];
 
+  chunkedProjects: any[][] = [];
+
   slideConfig = { 
-    slidesToShow: 3, 
+    slidesToShow: 1, 
     slidesToScroll: 1, 
     dots: true, 
     infinite: false 
   };
+
+  ngOnInit() {
+    this.chunkedProjects = this.chunkArray(this.projects, 2);
+  }
+
+  chunkArray(myArray: any[], chunk_size: number) {
+    const results = [];
+    while (myArray.length) {
+      results.push(myArray.splice(0, chunk_size));
+    }
+    return results;
+  }
 }
